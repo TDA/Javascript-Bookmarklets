ac8a4c545a0da64a80d527b2e29bcec4ecd998e6
Sai Pc <schand31@asu.edu>
2015-12-27 19:38:24 +0530

    Facebook automatic sharing script

---
+++ FacebookShares.js
@@ -0,0 +1,9 @@
+/**
+ * Created by schandramouli on 12/27/15.
+ */
+
+javascript:
+var shares = document.getElementsByClassName('share_action_link');
+for (var i = 0; i < shares.length; i++) {
+  shares[i].click();
+}
