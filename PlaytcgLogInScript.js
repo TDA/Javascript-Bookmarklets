0039b1720cf40a75b9c67457e8b30bd87536864b
Sai Pc <schand31@asu.edu>
2015-12-27 19:47:02 +0530

    Log in script for playtcg

---
+++ PlaytcgLogInScript.js
@@ -0,0 +1,10 @@
+/**
+ * Created by schandramouli on 12/27/15.
+ */
+
+javascript: var uname = document.getElementById('TextBoxUsername2');
+uname.value = 'saiprash_thegreatest@yahoo.co.in';
+var pass = document.getElementById('TextBoxPassword2');
+pass.value = 'dragon';
+var log = document.getElementById('loginbtnlogin');
+log.click();
