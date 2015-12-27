ad50b8b39e72c8c7c75ccbce5d47cc15095db75c
Sai Pc <schand31@asu.edu>
2015-12-27 19:37:32 +0530

    Facebook automatic liker

---
+++ FacebookLiker.js
@@ -0,0 +1,9 @@
+/**
+ * Created by schandramouli on 12/27/15.
+ */
+
+javascript:
+var likes = document.getElementsByClassName('UFILikeLink');
+for (var i = 0; i < likes.length; i++) {
+  likes[i].click();
+}
